44964c24a4c692315b7a09a602195e70eee10c38
CarinaCurielAlaniz <curielal@usc.edu>
2025-06-02 23:58:44 -0700

    updated base project for basic routing

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const Saved = () => {
+    return (
+        <View className={"flex-1 justify-center items-center"}>
+            <Text className={"text-5xl text-blue-500 font-bold"}>Saved</Text>
+        </View>
+    )
+}
+export default Saved;
